aa1bd3805eb81c1b09dfbb2667c1c5ce1890d0aa
jakalaka <jakalaka@aliyun.com>
2017-10-29 22:46:24 +0800

    ~~

---
+++ src/main/java/Server/Table/Model/CXTCS.java
@@ -0,0 +1,13 @@
+package Server.Table.Model;
+
+public class CXTCS extends CXTCSKey {
+    private String VALUE_;
+
+    public String getVALUE_() {
+        return VALUE_;
+    }
+
+    public void setVALUE_(String VALUE_) {
+        this.VALUE_ = VALUE_;
+    }
+}
